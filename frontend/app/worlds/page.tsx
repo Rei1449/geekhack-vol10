bf970e383717ea0c427dd4a361e8fbc6d74ce0ed
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-07-27 16:58:58 +0900

    add: 認証機能完了

--- frontend/app/worlds/page.tsx
+++ frontend/app/worlds/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function Page() {
-	return <div></div>;
+	return <div>worlds page</div>;
 }
