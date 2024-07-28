09a3f9e2ffd37f8156c08b2b196da927975bc9af
Rei1449 <kiriya4673@gmail.com>
2024-07-28 13:07:58 +0900

    トップページにアプリの説明を追加

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,6 +9,12 @@ export default function Home() {
 		// <SessionProvider>
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<Link href="/worlds">room選択する</Link>
+			<h1>みんなのTech広場</h1>
+			<div>
+				<p>みんなのTech広場とは？</p>
+				<p>バーチャルで繋がるでエンジニア同士の交流の場!</p>
+				<p>興味がある部屋に入ることで気軽にエンジニア同士交流できる</p>
+			</div>
 			{/* <TestuserProfile /> */}
 		</main>
 		// {/* </SessionProvider> */}
